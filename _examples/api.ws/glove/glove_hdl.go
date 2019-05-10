43fb81eef750e1674dd3858ae8f1b53d31c0e837
jinbanglin <17golang@gmail.com>
2019-05-10 13:20:17 +0800

    update

--- _examples/api.ws/glove/glove_hdl.go
+++ _examples/api.ws/glove/glove_hdl.go
@@ -1,15 +1,18 @@
 package glove
 
 import (
-  "moss/general/message"
   "context"
   "github.com/jinbanglin/go-ws"
   "github.com/gogo/protobuf/proto"
   "github.com/jinbanglin/go-ws/ws_proto"
+  "github.com/jinbanglin/micro/message"
 )
 
 func SendMsg(ctx context.Context, client *go_ws.Client, req proto.Message) (rsp proto.Message, err error) {
-  userLoad, sendPacket := req.(*ws_proto.SendMsgTestReq), &ws_proto.SendMsgTestRsp{Message: &message.MsgServerSuccess}
+  userLoad, sendPacket := req.(*ws_proto.SendMsgTestReq), &ws_proto.SendMsgTestRsp{Message: &msg.Message{
+    Code: 200,
+    Msg:  "hello go websocket!",
+  }}
   go_ws.BroadcastToOne(ctx, 10001, userLoad.UserId, "", client, sendPacket)
   return sendPacket, nil
 }
